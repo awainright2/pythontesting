3523b01a939ce2fa33876f46aae6d6b0df969b10
Adam <wainriac@guc.com>
2024-01-24 14:12:08 +0000

    adding sql connection testing

--- func.py
+++ func.py
@@ -1,6 +1,7 @@
 import io
 import json
 import logging
+import pyodbc
 
 from fdk import response
 
@@ -14,8 +15,28 @@ def handler(ctx, data: io.BytesIO=None):
         logging.getLogger().info('error parsing json payload: ' + str(ex))
 
     logging.getLogger().info("Inside Python Hello World function")
+    isConnected = sqlConnection()
     return response.Response(
         ctx, response_data=json.dumps(
-            {"Message": "Hey there are you: {0}".format(name)}),
+            {"Message": "{0}".format(isConnected)}),
         headers={"Content-Type": "application/json"}
     )
+
+def sqlConnection():
+    server = '10.23.9.206'
+    database = 'Apps'
+    username = 'wainriac-lsa'
+    password = 'mYaPP1ei$GolDen'
+
+    try:
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+',1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+        # Additional code for executing SQL statements or working with the database goes here
+        # For example: cursor = conn.cursor()
+        #              cursor.execute('SELECT * FROM your_table')
+        #              rows = cursor.fetchall()
+        #              print(rows)
+        conn.close()  # Close the connection when done
+    except pyodbc.Error as ex:
+        return "An error occurred:", ex
+        # Handle the error as per your application's requirements
+     return "Connection successful"
